fd509bdf53dfaa0f6742fa6b61038e5f71c56bb8
salty-max <max@jellycat.fr>
2023-06-26 17:27:05 +0200

    feat: implements basic lexer

--- token/token.go
+++ token/token.go
@@ -1,10 +1,22 @@
-package Token
+package token
 
 type TokenType string
 
 type Token struct {
 	Type    TokenType
 	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
 }
 
 const (
