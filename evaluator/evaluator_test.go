09d36252ee687121e6323a33289a4ba6d9c2df6e
salty-max <max@jellycat.fr>
2023-06-30 19:47:25 +0200

    feat: implements variable assignment

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -132,6 +132,22 @@ func TestIfElseExpressions(t *testing.T) {
 	}
 }
 
+func TestLetStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let x = 5; x;", 5},
+		{"let x = 5 * 5; x;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
+
 func TestReturnStatement(t *testing.T) {
 	tests := []struct {
 		input    string
@@ -199,6 +215,10 @@ func TestErrorHandling(t *testing.T) {
 			`,
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
+		{
+			"foobar",
+			"identifier not found: foobar",
+		},
 	}
 
 	for _, tt := range tests {
@@ -220,8 +240,9 @@ func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
+	env := object.NewEnvironment()
 
-	return Eval(program)
+	return Eval(program, env)
 }
 
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
